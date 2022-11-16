5a1e9ccb2ca5fb140708a5e1c4ecb75b6ca797fc
김민지 <ddaa637878@gmail.com>
2022-11-16 16:06:28 +0900

    avatar moeyomeske map

--- src/screens/food/Moeyomenske.js
+++ src/screens/food/Moeyomenske.js
@@ -73,19 +73,16 @@ function Moeyomenske() {
               marginTop: "10px",
             }}
           >
-            <b>오사카성</b>
+            <b>모에요멘스케</b>
           </h1>
 
           <div style={{ paddingTop: "40px" }}>
-            일본 〒540-0002 Osaka, Chuo Ward, Osakajo, １番１号
+          5 Chome-12-21 Fukushima, Fukushima Ward, Osaka, 553-0003 일본
           </div>
-          <div style={{ paddingTop: "20px" }}>전화번호: +81 6-6941-3044</div>
-          <div style={{ paddingTop: "20px" }}>성 종류: 아즈키-모모야마 양식의 성</div>
-          <div style={{ paddingTop: "20px" }}>
-            입장료: 성인: 600엔, 만 15세 이하 어린이는 무료입니다.
-          </div>
+          <div style={{ paddingTop: "20px" }}>전화번호: +81 6-6452-2101</div>
+          <div style={{ paddingTop: "20px" }}>가격: 950~1,200엔</div>
           <div style={{ paddingTop: "20px" }}>주차공간: 없음</div>
-          <div style={{ paddingTop: "20px" }}>성내 박물관 영업시간: am 9:00 ~ pm 17:00</div>
+          <div style={{ paddingTop: "20px" }}>영업시간:  11:00 ~ 15:30 (화요일 휴무, 일요일만 16시 마감)</div>
         </Col>
       </Row>
       <Row>
@@ -94,11 +91,12 @@ function Moeyomenske() {
             <b>설명</b>
           </h3>
           <div style={{ paddingTop: "20px", fontSize: "20px", lineHeight: "50px" }}>
-            일본에서는 수많은 영화의 소설 속에서 각색되는 도요토미 히데요시의 오사카성. 요새입니다.
-            해자를 깊이 파서 적이 쉽사리 넘어 들어올 수 없게하고 높은 누각을 지어 적의 침입을 빠르게
-            알아낼 수 있도록 하였습니다. 1583년 히데요시가 지어올린 이 성은 5층의
-            데슈카쿠(천수각)에, 기와까지 히데요시가 좋아하던 금박을 입혀서 당시 눈부시게 화려한
-            성이었다고 합니다.
+            한국에 국밥이 있다면 일본에는 라멘이 있다. 다양한 종류의 국밥이 있듯 라멘의 세계도 다채롭다,
+            어떤 육수인지, 무엇으로 간을 하는지, 면의 굵기, 면을 먹는 방식(국물·비빔·찍먹) 등을 조합하고, 
+            각 가게별 비법이 들어가면 서로 다른 수천 개의 라멘이 완성된다.오사카 현지인들을 줄 세우는 
+            모에요멘스케는 오리와 닭을 활용해 깔끔하면서도 깊은 육수를 뽑아내는 걸로 유명하다. 쫄깃한 면과, 
+            그 면을 살포시 덮고 있는 부드럽고 적당히 기름진 차슈도 매력 포인트. 350엔(한화 약 3,500원)으로 
+            맛볼 수 있는 차슈 덮밥도 꼭 먹어봐야 한다. 
           </div>
         </Col>
         <Col lg={7}>
@@ -106,8 +104,7 @@ function Moeyomenske() {
             <b>오시는 길</b>
           </h3>
           <div>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.750019870646!2d135.5237758151773!3d34.6862580914994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e0cd5c283afd%3A0xf01d07d5ca11e41!2z7Jik7IKs7Lm0IOyEsQ!5e0!3m2!1sko!2skr!4v1668428005853!5m2!1sko!2skr"
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1126.7635711502076!2d135.486736463719!3d34.6962518362416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e73d8ea37a69%3A0xd319b6d6c0d798c5!2z66qo7JeQ7JqUIOupmOyKpOy8gA!5e0!3m2!1sko!2skr!4v1668582261634!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
               width="600"
               height="450"
               allowfullscreen=""
